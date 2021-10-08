3d5de36c652f104c1e006f2dc2fefb99d19da059
Navya Rajput <navyasrajput@gmail.com>
2021-10-07 21:15:24 -0400

    Responsive Design and code clean up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,23 +5,30 @@
  */
 $(document).ready(function() {
 
+  /*
+   * Convert the string to a text node
+   * Returns the inner HTML of the created text node
+   */
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
-  const createTweetElement = function(tweetObj) {
+  /*
+   * Converts tweet object to html article elements
+   */
+  const createTweetElement = function(tObj) {
     const $tweet = $("<article>").addClass("tweet");
-    const daysAgo = daysSinceTweet(tweetObj["created_at"])
+    const daysAgo = daysSinceTweet(tObj["created_at"]);
 
     const innerHTML = `
           <header>
-              <img src= ${tweetObj.user.avatars}>
-              <span>${tweetObj.user.name}</span>
-              <span class="handle">${tweetObj.user.handle}</span>
+              <img src= ${tObj.user.avatars}>
+              <span>${tObj.user.name}</span>
+              <span class="handle">${tObj.user.handle}</span>
           </header>
-           <span>${escape(tweetObj.content.text)}</span>
+           <span>${escape(tObj.content.text)}</span>
           <footer>
           <span>${daysAgo} days ago</span>
           <span class="interactOptions"><i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
@@ -31,6 +38,7 @@ $(document).ready(function() {
     $tweet.append(innerHTML);
     return $tweet;
   };
+
   const daysSinceTweet = function(epochOfTweet) {
     const currentDate = new Date();
     const currentTime = currentDate.getTime();
@@ -40,40 +48,48 @@ $(document).ready(function() {
     const dayDifference = timeDifference/millisecondsInDay;
 
     return Math.floor(dayDifference);
-  }
+  };
 
-  const renderTweets = function(tweetObjArr) {
-    for (const tweet of tweetObjArr) {
+  const renderTweets = function(tObjArr) {
+    for (const tweet of tObjArr) {
       const $tweet = createTweetElement(tweet);
       $('section.all-tweets').prepend($tweet);
     }
   };
 
-  //renderTweets(data);
-
+   /**
+   * Makes a GET request to retrieve tweet history
+   * Returns array of tweets
+   */
   const loadTweets = function () {
     $.ajax('/tweets', { method: 'GET' })
     .then(function (allTweets) {
-    //  console.log(allTweets);
       renderTweets(allTweets);
     })
-  }
+  };
 
   loadTweets();
 
+   /**
+   * Triggered on submission of the form with the class 'new-tweet'
+   * Validate tweet length and display error message if required.
+   * makes a POST request with form text to /tweets/
+   * and then a GET request to /tweets/
+   * Add new tweet to the page.
+   */
   $('.new-tweet form').submit( function (event) {
     event.preventDefault();   
     $('.new-tweet p').empty().slideUp();
     const $form = $(this);
-    const newTweetTextStr = $form.children('textarea').val();
+    const newTweetStr = $form.children('textarea').val();
 
-    if (!newTweetTextStr) {
+    if (!newTweetStr) {
       $('.new-tweet p').append('<b>Error:</b> Tweet is empty! add some tweet.');
       setTimeout(() => {
         $('.new-tweet p').slideDown();
       }, 600);
 
-    } else if (newTweetTextStr.length > 140) { 
+    } else if (newTweetStr.length > 140) { 
       $('.new-tweet p').append("<b>Error:</b> Max 140 characteres are allowed!");
       setTimeout(() => {
         $('.new-tweet p').slideDown();
@@ -83,20 +99,20 @@ $(document).ready(function() {
       $('.new-tweet p').slideUp();
       const tweet = $form.serialize();
       $.ajax({ url: "/tweets/", method: 'POST', data: tweet })
-      .then (function (postRequestReturnValue) {
+      .then (function () {
         return $.ajax('/tweets', { method: 'GET' })
       })
-      .then (function (getRequestReturnValue) {
+      .then (function (getReqArr) {
         $form[0].reset();
         $form.children('span').text(140);
-        const latestTweet = [getRequestReturnValue[getRequestReturnValue.length - 1]];
-        renderTweets(latestTweet);
+        const newTweet = [getReqArr[getReqArr.length - 1]];
+        renderTweets(newTweet);
       })
       .fail(function (err) {
         alert(err.responseJSON.error);
       })
     }
-  })
+  });
 
   $('#compose-new button').click(function () {
     $('section.new-tweet').slideToggle("slow");
